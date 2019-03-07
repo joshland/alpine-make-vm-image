caf5f66873adaca4a10e6a2948dc3059b2d0f9d0
Joshua Schmidlkofer <joshua@joshuainnovates.us>
2019-03-07 12:08:15 -0800

    Update: build for serial + routing

--- rbuild.sh
+++ rbuild.sh
@@ -1,1 +1,1 @@
-./alpine-make-vm-image --rootfs btrfs -p router/packages -r router/repositories -c -f qcow2 --mirror-uri https://mirrors.gigenet.com/alpinelinux router.qcow2 router/configure.sh
+./alpine-make-vm-image --rootfs btrfs -p "$(cat router/packages)" -r router/repositories -c -f qcow2 --mirror-uri https://mirrors.gigenet.com/alpinelinux router.qcow2 router/configure.sh
--- router/configure.sh
+++ router/configure.sh
@@ -8,15 +8,64 @@ step() {
 
 
 step 'Set up timezone'
-setup-timezone -z US/Los_Angeles
+setup-timezone -z US/Pacific
 
 step 'Set up networking'
 cat > /etc/network/interfaces <<-EOF
 	iface lo inet loopback
 	iface eth0 inet dhcp
+	iface eth1 inet dhcp
+	iface eth2 inet dhcp
 EOF
+
+cat > /etc/quagga/ospfd.conf <<EOF
+! -*- ospf -*-
+!
+! OSPFd sample configuration file
+!
+!
+hostname ospfd
+password zebra
+!enable password please-set-at-here
+!
+!router ospf
+!  network 192.168.1.0/24 area 0
+!
+log stdout
+
+EOF
+
+cat > /etc/quagga/zebra.conf <<EOF
+! -*- zebra -*-
+!
+! zebra sample configuration file
+!
+! $Id: zebra.conf.sample,v 1.1 2002/12/13 20:15:30 paul Exp $
+!
+hostname Router
+password zebra
+enable password zebra
+!
+! Interface's description. 
+!
+!interface lo
+! description test of desc.
+!
+!interface sit0
+! multicast
+!
+! Static default route sample.
+!
+!ip route 0.0.0.0/0 203.181.89.241
+!
+!log file zebra.log
+
+EOF
+
 ln -s networking /etc/init.d/net.lo
 ln -s networking /etc/init.d/net.eth0
+ln -s networking /etc/init.d/net.eth1
+ln -s networking /etc/init.d/net.eth2
 
 step 'Adjust rc.conf'
 sed -Ei \
@@ -30,7 +79,9 @@ rc-update add acpid default
 rc-update add chronyd default
 rc-update add crond default
 rc-update add net.eth0 default
+rc-update add net.eth1 default
+rc-update add net.eth2 default
 rc-update add net.lo boot
 rc-update add termencoding boot
 rc-update add crond default
-rc-update add quagga default
+rc-update add zebra default
